9f22f9750d6ccc645150ed954176d3c3a1cc8acf
xviubu <xviubu@gmail.com>
2014-04-02 16:25:32 +0800

    新增主页Controller

---
+++ src/main/java/com/xviubu/personalblog/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.xviubu.personalblog.web;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping({"/","/index"})
+	public String enterIndexPage(Map<String,Object> model){
+		return "index";
+	}
+}
